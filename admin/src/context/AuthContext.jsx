1d195408b5bf487d78bd3fa497adac967097f3c0
krishn6772 <140115143+krishn6772@users.noreply.github.com>
2025-07-05 14:42:10 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from 'react'
 
 export const authDataContext = createContext();
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:5000";
+    let serverUrl = "https://shope-backend-5mrl.onrender.com";
 
     let value = {
         serverUrl,
